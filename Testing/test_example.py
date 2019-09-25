d79ca90c4405417b27748331377cdc4c2a48a69a
amark02 <alexander.mark20@ycdsbk12.ca>
2019-09-25 10:36:09 -0400

    using pytest

---
+++ Testing/test_example.py
@@ -0,0 +1,13 @@
+"""
+def test_teting():
+    assert True == True
+
+    just makes sure pytest is working on this file
+"""
+
+def add(a, b):
+    return a + b
+
+def test_add():
+    assert add(1, 1) == 2
+    assert add(3, 4) == 7
---
+++ Testing/test_math_functions.py
@@ -0,0 +1,11 @@
+import math_functions #(don't need '.py' at the end)
+
+# math_functions.add() (allows you to use the function from the other file)
+
+def test_add():
+    assert math_functions.add(1, 1) == 2
+    assert math_functions.add(3, 5) == 8
+
+
+def test_subract():
+    assert math_functions.subract(5, 2) == 3
