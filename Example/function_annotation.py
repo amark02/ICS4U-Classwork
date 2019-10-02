49c1438f8d8bdbec8c58d729b21e3eb15c745d49
amark02 <alexander.mark20@ycdsbk12.ca>
2019-10-02 11:10:16 -0400

    updates

---
+++ Example/function_annotation.py
@@ -0,0 +1,30 @@
+
+def function_name(param1: type, param2: type) -> return_type:
+    """Sort description of the function
+
+    Args:
+        param1: Explain the first parameter
+        param2: Explain the second parameter.
+    Returns:
+        Explain what the function returns
+    """
+    pass
+
+"""the java way of doing a function
+
+public static functionName(int a, int b) {
+
+} 
+"""
+
+# sum
+
+# math.ceil
+
+# sum math.floor
+
+# Function that takes a name and outputs a message
+
+# Function that takes two floats and finds their average
+
+# Function that takes an age and returns True if they are of voting age
---
+++ Testing/test_existing_stuff.py
@@ -0,0 +1,26 @@
+import math
+
+
+
+# test sum(numbers: List[float]) -> float
+def test_sum():
+    some_list = [1, 2, 3]
+    assert sum(some_list) == 6
+    assert sum([]) == 0
+    assert sum([5, 5, 5]) == 15
+    assert sum([1] * 1000) == 1000
+
+# test math.floor(float) -> int
+def test_math_floor():
+    assert math.floor(6.001) == 6
+    assert math.floor (6.0) == 6
+    assert math.floor (6.999) == 6
+
+# test math.ceil(float) -> int
+def test_math_ceil():
+    assert math.ceil(6.001) == 7
+    assert math.ceil(6.0) == 6
+    assert math.ceil (6.999) == 7
+
+# test f strings
+# test .format()
