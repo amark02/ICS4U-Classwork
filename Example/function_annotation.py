fca244c553a775f97c2fcda7241b5b28c584d05e
amark02 <alexander.mark20@ycdsbk12.ca>
2019-10-03 10:44:30 -0400

    new stuff

--- Example/function_annotation.py
+++ Example/function_annotation.py
@@ -18,13 +18,59 @@ public static functionName(int a, int b) {
 """
 
 # sum
+def sum(numbers: List[float]) -> float:
+    """Adds up all numbers in a list.
+
+    Args:
+        numbers: A list of numbers
+    Returns:
+        The sum of the numbers in a list.
+    """
+    pass
 
 # math.ceil
+def ceil(num: float) -> int:
+    """Rounds a decimal number up to the nearest greater interger
 
-# sum math.floor
+    Args:
+        num: A decimal number
+    Returns:
+        The number rounded up.
+    """
+    pass
+
+# math.floor
 
 # Function that takes a name and outputs a message
+def create_message(name: str) -> str:
+    """Returns a personalized message
+
+    Args:
+        name: person's name
+    Returns:
+        Pesonalized message including someone's name.
+    """
+    pass
 
 # Function that takes two floats and finds their average
+def avg(a: float, b: float) -> float:
+    """Gets the average of two numbers.
 
-# Function that takes an age and returns True if they are of voting age
+    Args:
+        a: A number
+        b: Another number
+    Returns:
+        The average of the two numbers.
+    """
+    pass
+
+# Function that takes an age and returns True if they are of voting ages
+def can_vote(age: int) -> bool
+    """Determines whether or not a person can vote
+
+    Args:
+        age: someone's age
+    Return:
+        True if they can vote, False if they can't
+    """
+    pass
--- Testing/test_existing_stuff.py
+++ Testing/test_existing_stuff.py
@@ -13,7 +13,7 @@ def test_sum():
 # test math.floor(float) -> int
 def test_math_floor():
     assert math.floor(6.001) == 6
-    assert math.floor (6.0) == 6
+    assert math.floor (4.5) == 4
     assert math.floor (6.999) == 6
 
 # test math.ceil(float) -> int
@@ -23,4 +23,13 @@ def test_math_ceil():
     assert math.ceil (6.999) == 7
 
 # test f strings
+def test_f_strings():
+    assert f"hello {3+5}" == "hello 8"
+
+    a, b = 3, 4
+    assert f"result: {a+b}" == "result: 7"
+
 # test .format()
+def test_dot_format():
+    assert "{} {} {}".format(1, 2, 3) == "1 2 3"
+    assert "blah {} blah".format("Happy") == "blah Happy blah"
