fe8d8cb7dc28f2ddc53870f95d09ffaaa9d27681
amark02 <alexander.mark20@ycdsbk12.ca>
2019-12-09 10:54:11 -0500

    recursion

---
+++ Algorithms/recursion_practice.py
@@ -0,0 +1,40 @@
+"""
+# Factorial
+def factorial(n: int) -> int:
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return n * factorial(n-1)
+print(factorial(1))
+print(factorial(2))
+print(factorial(3))
+"""
+"""
+# bunnyEars
+def bunnyEars(n: int) -> int:
+    if n == 0:
+        return 0
+    return 2 + bunnyEars(n-1)
+print(bunnyEars(0))
+print(bunnyEars(1))
+print(bunnyEars(2))
+"""
+
+# Fibonacci
+def fibonacci(n: int) -> int:
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+print(fibonacci(0))
+print(fibonacci(1))
+print(fibonacci(2))
+
+# strCount
+def strCount(string: str, ksub: str) -> int:
+    if string == "":
+        return 0
+    if 
---
+++ Algorithms/test_recursion_practice.py
@@ -0,0 +1,1 @@
+import recursion_practice import 
